533c6c32403bd692a1d5e3150d8c3ad642b5cded
skywardly <skywandou@gmail.com>
2020-07-23 18:49:30 +0900

    add auto comments update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       var html =
-      `<div class="main-chat__message-list__box">
+      `<div class="main-chat__message-list__box" data-message-id=${message.id}>
         <div class="main-chat__message-list__box__username">
           ${message.user_name}
         </div>
@@ -19,7 +19,7 @@ $(function(){
       return html;
     } else {
     var html =
-      `<div class="main-chat__message-list__box">
+      `<div class="main-chat__message-list__box" data-message-id=${message.id}>
         <div class="main-chat__message-list__box__username">
           ${message.user_name}
         </div>
@@ -59,4 +59,37 @@ $(function(){
       $('.input__box__submit').attr('disabled', false);
     });
   });
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.main-chat__message-list__box:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.main-chat__message-list').append(insertHTML);
+        $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
